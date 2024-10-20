310e519c32a74b39f346d3fd07f2ab098f546de6
Disura Sandaruwan <disurasandaruwan0@gmail.com>
2024-10-20 19:36:20 +0530

    "Delete Function Category" created

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -31,3 +31,33 @@ export function createCategory(req, res){
         }
     )
 }
+
+//------------------------------Delete Category--------------------------------
+export function deleteCategory(req, res){
+    if(req.user == null){
+        res.status(401).json({
+            message: "Unauthorized"
+        })
+        return
+    }
+    if(req.user.type != "Admin"){
+        res.status(403).json({
+            message: "Forbidden"
+        })
+        return
+    }
+    const name = req.params.name
+    Category.findOneAndDelete({name: name}).then(
+        ()=>{
+            res.json({
+                message: "Category Deleted Successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message: "Category Deletion Failed"
+            })
+        }
+    )
+}
